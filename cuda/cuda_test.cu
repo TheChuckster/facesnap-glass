b5da7619b3820759bf9e0c032d49b99ee9870d58
Charles Moyes <me@charlesmoyes.com>
2013-07-21 21:38:18 -0700

    Test CUDA kernel

--- cuda/cuda_test.cu
+++ cuda/cuda_test.cu
@@ -1,7 +1,6 @@
 #include <stdio.h>
  
-const int N = 16; 
-const int blocksize = 16; 
+const int N = 16;
  
 __global__ 
 void hello(char *a, int *b) 
@@ -14,25 +13,26 @@ int cuda_test()
 	char a[N] = "Hello \0\0\0\0\0\0";
 	int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
  
-	char *ad;
-	int *bd;
-	const int csize = N*sizeof(char);
-	const int isize = N*sizeof(int);
+	char* ad = NULL;
+	int* bd = NULL;
  
 	printf("%s", a);
  
-	cudaMalloc( (void**)&ad, csize ); 
-	cudaMalloc( (void**)&bd, isize ); 
-	cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
-	cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
+	cudaMalloc((void**)&ad, sizeof(a)); 
+	cudaMalloc((void**)&bd, sizeof(b)); 
+
+	cudaMemcpy(ad, a, sizeof(a), cudaMemcpyHostToDevice); 
+	cudaMemcpy(bd, b, sizeof(b), cudaMemcpyHostToDevice); 
 	
-	dim3 dimBlock( blocksize, 1 );
-	dim3 dimGrid( 1, 1 );
+	dim3 dimBlock(N, 1);
+	dim3 dimGrid(1, 1);
 	hello<<<dimGrid, dimBlock>>>(ad, bd);
-	cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
-	cudaFree( ad );
-	cudaFree( bd );
+	cudaMemcpy(a, ad, sizeof(a), cudaMemcpyDeviceToHost); 
+
+	cudaFree(ad);
+	cudaFree(bd);
 	
 	printf("%s\n", a);
+	
 	return EXIT_SUCCESS;
 }
